ff4948cce0b954d46489896aa7f0227ad11bc6ae
Rahiq-Mac <rahiq-mac@Rahiq-Macs-Mac-mini.local>
2026-04-22 16:13:34 +0600

    Use more lenient static files storage to prevent deployment failures

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Railway/Whitenoise best practice
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
